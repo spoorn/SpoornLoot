cb0d4327dc17db51ed775bc39981d45593bd9781
spoorn <michael.trinh1996@gmail.com>
2021-03-08 13:10:45 -0800

    Bump to 1.3, add heart sword sounds

--- src/main/java/org/spoorn/spoornloot/item/swords/HeartSwordItem.java
+++ src/main/java/org/spoorn/spoornloot/item/swords/HeartSwordItem.java
@@ -1,15 +1,19 @@
 package org.spoorn.spoornloot.item.swords;
 
-import lombok.extern.log4j.Log4j2;
-import net.minecraft.item.ToolMaterial;
-import net.minecraft.item.ToolMaterials;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.Rarity;
-import org.spoorn.spoornloot.util.rarity.SpoornRarity;
-import org.spoorn.spoornloot.util.settings.SpoornItemSettings;
-
 import static org.spoorn.spoornloot.util.SpoornUtil.MODID;
 import static org.spoorn.spoornloot.util.SpoornUtil.SPOORN_ITEM_GROUP;
+import lombok.extern.log4j.Log4j2;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.*;
+import net.minecraft.world.World;
+import org.spoorn.spoornloot.sounds.SpoornSoundsUtil;
+import org.spoorn.spoornloot.util.rarity.SpoornRarity;
+import org.spoorn.spoornloot.util.settings.SpoornItemSettings;
 
 @Log4j2
 public class HeartSwordItem extends BaseSpoornSwordItem {
@@ -31,5 +35,45 @@ public class HeartSwordItem extends BaseSpoornSwordItem {
 
     public HeartSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, DEFAULT_SPOORN_RARITY, settings);
+        registerSounds();
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        if (!world.isClient()) {
+            if (stack.getCooldown() == 0) {
+                world.playSound(
+                    null,
+                    user.getBlockPos(),
+                    SpoornSoundsUtil.SM_THEME_SOUND,
+                    SoundCategory.PLAYERS,
+                    0.6f,
+                    1f
+                );
+                stack.setCooldown(600);
+                return TypedActionResult.success(stack);
+            }
+        }
+        return TypedActionResult.pass(stack);
+    }
+
+    private void registerSounds() {
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            if (!world.isClient()) {
+                Item item = player.getMainHandStack().getItem();
+                if (item instanceof HeartSwordItem && entity.isLiving()) {
+                    world.playSound(
+                        null,
+                        player.getBlockPos(),
+                        SpoornSoundsUtil.SM_WAND_SOUND,
+                        SoundCategory.PLAYERS,
+                        0.4f,
+                        1f
+                    );
+                }
+            }
+            return ActionResult.PASS;
+        });
     }
 }
--- src/main/java/org/spoorn/spoornloot/sounds/SpoornSoundsUtil.java
+++ src/main/java/org/spoorn/spoornloot/sounds/SpoornSoundsUtil.java
@@ -9,9 +9,13 @@ public class SpoornSoundsUtil {
 
     public static final Identifier SPOORN_SWORD_HIT_CHAR_ID = new Identifier(SpoornUtil.MODID, "spoorn_sword_hit_char");
     public static final Identifier SPOORN_SWORD_HIT_ENV_ID = new Identifier(SpoornUtil.MODID, "spoorn_sword_hit_env");
+    public static final Identifier SM_WAND_ID = new Identifier(SpoornUtil.MODID, "sm_wand");
+    public static final Identifier SM_THEME_ID = new Identifier(SpoornUtil.MODID, "sm_theme");
 
     public static final SoundEvent SPOORN_SWORD_HIT_CHAR_SOUND = registerSoundEvent(new SoundEvent(SPOORN_SWORD_HIT_CHAR_ID));
     public static final SoundEvent SPOORN_SWORD_HIT_ENV_SOUND = registerSoundEvent(new SoundEvent(SPOORN_SWORD_HIT_ENV_ID));
+    public static final SoundEvent SM_WAND_SOUND = registerSoundEvent(new SoundEvent(SM_WAND_ID));
+    public static final SoundEvent SM_THEME_SOUND = registerSoundEvent(new SoundEvent(SM_THEME_ID));
 
     private static SoundEvent registerSoundEvent(SoundEvent soundEvent) {
         return Registry.register(Registry.SOUND_EVENT, soundEvent.getId(), soundEvent);
