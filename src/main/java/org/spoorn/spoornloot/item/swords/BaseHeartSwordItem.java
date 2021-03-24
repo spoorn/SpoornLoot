fdd47c2f0ba9c7c10cadc73566906b222df7876b
spoorn <michael.trinh1996@gmail.com>
2021-03-24 13:04:15 -0700

    Bump to 1.9.2
    Change heart particles from heart swords to be server side

--- src/main/java/org/spoorn/spoornloot/item/swords/BaseHeartSwordItem.java
+++ src/main/java/org/spoorn/spoornloot/item/swords/BaseHeartSwordItem.java
@@ -2,12 +2,12 @@ package org.spoorn.spoornloot.item.swords;
 
 import lombok.extern.log4j.Log4j2;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -65,14 +65,13 @@ abstract class BaseHeartSwordItem extends BaseSpoornSwordItem {
                     );
                     compoundTag.putFloat(SpoornUtil.LAST_SPOORN_SOUND, currTime);
                 }
-            } else if (rightSituation && world.isClient()) {
+
                 for (int i = 0; i < 2; ++i) {
                     double d = SpoornUtil.RANDOM.nextGaussian() * 0.02D;
                     double e = SpoornUtil.RANDOM.nextGaussian() * 0.02D;
                     double f = SpoornUtil.RANDOM.nextGaussian() * 0.02D;
-                    // TODO: Add particles to server side
-                    world.addParticle(ParticleTypes.HEART, entity.getParticleX(1.0D),
-                            entity.getRandomBodyY() + 0.5D, entity.getParticleZ(1.0D), d, e, f);
+                    ((ServerWorld)world).spawnParticles(ParticleTypes.HEART, entity.getParticleX(1.0D),
+                            entity.getRandomBodyY() + 0.5D, entity.getParticleZ(1.0D), 1, d, e, f, 0);
                 }
             }
             return ActionResult.PASS;
